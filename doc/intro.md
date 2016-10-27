e2853d2f4c6e610670c6575160312a9413e69eea
Wes Morgan <wesmorgan@icloud.com>
2016-10-27 16:08:41 -0600

    lein template output

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to phantom-zone
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/phantom_zone/core_test.clj
@@ -0,0 +1,7 @@
+(ns phantom-zone.core-test
+  (:require [clojure.test :refer :all]
+            [phantom-zone.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
