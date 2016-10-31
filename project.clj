009c24c15c591661e68e12e51c20eb230b7d2fef
Wes Morgan <wesmorgan@icloud.com>
2016-10-31 09:36:09 -0600

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/phantom-zone "0.1.1-SNAPSHOT"
+(defproject democracyworks/phantom-zone "0.1.1"
   :description "Translates US ZIP codes into city, state, and timezone"
   :url "https://github.com/democracyworks/phantom-zone"
   :license {:name "Eclipse Public License"
